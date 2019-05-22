771ea482439d75336ac29c4ceffa62d3b5996fef
Stuart Whalley <50208733+swhalley9@users.noreply.github.com>
2019-05-22 14:29:10 -0600

    Update step_4.py

--- step_4.py
+++ step_4.py
@@ -2,4 +2,22 @@ from step_1 import transform_products_to_list
 
 
 def calculate_total_per_invoices(products_string):
-    pass
+    products_list = transform_products_to_list(products_string)
+    
+    results = {}
+    
+    for product in products_list:
+        customer_id = product[-2]
+        invoice_id = product[0]
+        quantity = float(product[3])
+        cost = float(product[-3])#because they are strings
+        
+        line_price = round(quantity * cost,3)
+        
+        results.setdefault(customer_id,{})
+        results[customer_id].setdefault(invoice_id,0)
+        
+        results[customer_id][invoice_id] += line_price
+    
+    
+    return results
